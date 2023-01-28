596243909c0a5086e8b2613db15de39c1e29c773
Paul Amago <apaulo2035@gmail.com>
2023-01-28 14:33:56 -0700

    styles and scripts

---
+++ script/landing.js
@@ -0,0 +1,7 @@
+const registerBtn = document.querySelector("#register-btn");
+
+const registerHandler = () => {
+    return alert("Bleh");
+}
+
+registerBtn.addEventListener('click', registerHandler);
