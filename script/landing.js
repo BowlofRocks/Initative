b75751166ecf199b7c8928387bf0c1e02562d2e6
Paul Amago <apaulo2035@gmail.com>
2023-02-07 18:55:38 -0700

    started pop-up stuff

--- script/landing.js
+++ script/landing.js
@@ -2,12 +2,20 @@ const registerBtn = document.querySelector("#register-btn");
 const signInButton = document.querySelector("#sign-in-button");
 
 const registerHandler = () => {
-    window.location.replace("https://bowlofrocks.github.io/Initative/initiative.html")
+    
 }
 
 const signInHandler = () => {
-    return alert("Bleh");
+    
 }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
 
 registerBtn.addEventListener('click', registerHandler);
 signInButton.addEventListener('click', signInHandler);
