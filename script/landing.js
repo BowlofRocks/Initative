8191ec1f0fe4eec8c336cd64402545533bf403db
Paul Amago <apaulo2035@gmail.com>
2023-02-07 18:23:05 -0700

    made some changes in  landing html

--- script/landing.js
+++ script/landing.js
@@ -1,7 +1,15 @@
 const registerBtn = document.querySelector("#register-btn");
+const signInButton = document.querySelector("#sign-in-button");
 
 const registerHandler = () => {
+    window.location.replace("http://localhost:8080/initiative.html")
+}
+
+const signInHandler = () => {
     return alert("Bleh");
 }
 
 registerBtn.addEventListener('click', registerHandler);
+signInButton.addEventListener('click', signInHandler);
+
+
