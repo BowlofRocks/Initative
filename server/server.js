f6d0e9c5cc3983319d7f1cdbfb41be70417d717b
Paul Amago <apaulo2035@gmail.com>
2023-02-17 15:16:02 -0700

    forgot to push data for server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log("Worked")
+const express = require('express')
+
+
+const app = express()
+
+app.use(express.json())
+
+
+app.listen(4040, () => console.log('Server is running on 4040'))
+
