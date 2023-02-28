e0ebf1f29d02432837bc17e6278b58684de7f690
Paul Amago <apaulo2035@gmail.com>
2023-02-28 15:29:57 -0700

    figuring out modal and started database

--- script/landing.js
+++ script/landing.js
@@ -3,30 +3,44 @@ const signInButton = document.querySelector("#sign-in-button");
 const hideHeader = document.querySelector("header");
 const hideFooter = document.querySelector("footer");
 const hideAbout = document.querySelectorAll("section");
-
-$('.btn').button();
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+var snForm = document.getElementById("snForm")
 
 const registerHandler = () => {
-    
+  
 }
 
 const signInHandler = () => {
-    
+  
+}
+btn.onclick = function() {
+  snForm.style.display = "block";
 }
 
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 function signInOpenForm() {
-    document.getElementById("snForm").style.display = "block";
+  document.getElementById("snForm").style.display = "block";
     hideHeader.classList.add("gray-sign-in");
     hideFooter.classList.add("gray-sign-in");
     hideAbout.classList.add("gray-sign-in");
   }
   
 function signInCloseForm() {
-    document.getElementById("snForm").style.display = "none";
-    hideHeader.classList.remove("gray-sign-in");
-    hideFooter.classList.remove("gray-sign-in");
-    hideAbout.classList.remove("gray-sign-in");
-  }
+  document.getElementById("snForm").style.display = "none";
+  hideHeader.classList.remove("gray-sign-in");
+  hideFooter.classList.remove("gray-sign-in");
+  hideAbout.classList.remove("gray-sign-in");
+}
 
 function rgstrOpenForm() {
     document.getElementById("rgstrForm").style.display = "block";
@@ -42,3 +56,15 @@ registerBtn.addEventListener('click', registerHandler);
 signInButton.addEventListener('click', signInHandler);
 
 
+
+// Get the modal
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+
+// When the user clicks the button, open the modal 
+
+// When the user clicks on <span> (x), close the modal
+
+// When the user clicks anywhere outside of the modal, close it
