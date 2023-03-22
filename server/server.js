93397bff37218fb0555391c16a64e957ee2ed3a0
Kenton Olsen <kentonolsen@gmail.com>
2023-03-22 16:44:40 -0600

    test

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 // Holding the info from users and cast.
-
-
 const express = require('express')
 const app = express()
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'aws',
+    dialect: 'postgres'
+});
 const serverPort = 8080
 let { registerHandler, loginHandler } = require('./controllers/users');
 let { getCampaigns, getCharacters, getCampaigns } = require('./controllers/cast');
@@ -16,5 +19,5 @@ app.post('/api/login', loginHandler);
 
 app.get('/api/initiative', getCampaigns);
 
-app.listen(8080, () => console.log('Server is running on 8080'))
+app.listen(serverPort, () => console.log('Server is running on 8080'))
 
