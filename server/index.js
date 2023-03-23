1b7fe889417c98531c81d0b51cd58526741f15cc
Kenton Olsen <kentonolsen@gmail.com>
2023-03-23 16:15:01 -0600

    Added Data Models

--- server/index.js
+++ server/index.js
@@ -4,8 +4,70 @@ const sequelize = new Sequelize('initiative', 'postgresadmin', 'KastovKool-Aid',
     dialect: 'postgres'
 });
 
-sequelize.authenticate().then(() => {
-    console.log('Connected')
-}).catch((err) => {
-    console.log(err)
-});
+const User = sequelize.define('user', {
+    user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+const Campaign = sequelize.define('campaign', {
+    campaign_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    campaign_name: {
+        type: Sequelize.DataTypes.STRING
+    }
+})
+
+const Players = sequelize.define('players', {
+    player_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    player_name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    player_initiative: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    },
+    player_health: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    }
+})
+
+const nonPlayerCast = sequelize.define('non_player_cast', {
+    cast_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    cast_name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    cast_initiative: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    },
+    cast_health: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    }
+})
